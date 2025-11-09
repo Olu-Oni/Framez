714762501b08e9d4e4c5f2d25eed970c077320b5
Olu-Oni <oluomicheal8@gmail.com>
2025-11-09 15:13:54 +0100

    initialized project

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const index = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>index</Text>
+        
+      </View>
+      
+    </SafeAreaView>
+  );
+};
+
+export default index;
